ec3adb9fd773077b87ca58af481730a292ae9681
Braydon Coyer <braydon@coyer.net>
2022-12-13 17:37:54 -0600

    Removed Image from starting point

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -11,12 +11,6 @@ type Props = {
 export default function CustomLink({ children, href }: Props) {
   let [imagePreview, setImagePreview] = React.useState("");
   let [isHovering, setIsHovering] = React.useState(false);
-  let inImagePreview = false;
-  let inLink = false;
-
-  let handleMouseEnterImage = () => {};
-
-  let handleMouseLeaveImage = () => {};
 
   let handleMouseEnterLink = () => {};
 
@@ -46,30 +40,6 @@ export default function CustomLink({ children, href }: Props) {
         </Link>
 
         {/* Image Preview */}
-        {isHovering && (
-          <Link href={href} passHref>
-            <span
-              className="w-56 h-44 absolute top-[-195px] left-1/2 transform -translate-x-[7rem] translate-y-8 flex items-start justify-center"
-              onMouseEnter={handleMouseEnterImage}
-              onFocus={handleMouseEnterImage}
-              onMouseLeave={handleMouseLeaveImage}
-              onBlur={handleMouseLeaveImage}
-            >
-              {imagePreview ? (
-                <Image
-                  fill
-                  className="object-cover object-top w-56 h-40 bg-white rounded-md shadow-lg hover:ring-4 hover:ring-emerald-400"
-                  src={imagePreview}
-                  alt={children}
-                />
-              ) : (
-                <span className="flex items-center justify-center w-56 h-40 bg-white rounded-md shadow-lg">
-                  Loading...
-                </span>
-              )}
-            </span>
-          </Link>
-        )}
       </span>
 
       {/* For mobile devices */}
