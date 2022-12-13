dbc0e711d96fc79588f5ff4483418d6db9d7327d
Braydon Coyer <braydon@coyer.net>
2022-12-12 21:11:10 -0600

    Removing dependencies

---
+++ pages/api/link-preview.ts
@@ -0,0 +1,17 @@
+import mql from "@microlink/mql";
+
+export default async function handler(req, res) {
+  try {
+    let { url } = req.query;
+    const { data } = await mql(url, {
+      screenshot: true,
+    });
+    res.status(200).json({
+      image: data?.screenshot?.url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: JSON.stringify(error),
+    });
+  }
+}
